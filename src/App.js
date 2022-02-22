1dc702f5faf9d7bf737c508464a7763fbb1a93b1
Esmée Zootjes <esmeezootjes@gmail.com>
2022-02-22 15:44:34 +0100

    created the search form and played with some CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import logo from "./logo.png"
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+        <main>
+          <Dictionary/>
+        </main>
+      <footer className="text-center">
+        Coded by Esmée Zootjes
+      </footer>
+      </div>
     </div>
   );
 }
