896c06723531b08861f50bffb5f64495c71e365c
Esmée Zootjes <esmeezootjes@gmail.com>
2022-02-26 17:04:57 +0100

    Added the api for looking up the words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState("");
 
+    function showWord(response) {
+        console.log(response.data[0]);
+
+    }
+
     function Search(event) {
        
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`);  
+        alert(`Searching for ${keyword} definition`); 
+      
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(showWord)
     }
+
+    
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
@@ -17,7 +29,7 @@ export default function Dictionary () {
     return (
     <div className="Dictionary"> 
         <form onSubmit={Search}>
-            <input type="search" autofocus={true} onChange={handleKeywordChange}/>
+            <input type="search" autoFocus={true} onChange={handleKeywordChange}/>
         </form>
         </div>
         );
