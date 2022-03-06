0ba4ed140a205debda9577ea57f7fc12dd7f8d19
Esmée Zootjes <esmeezootjes@gmail.com>
2022-03-06 15:33:00 +0100

    small changes

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
   console.log(props.phonetic);
   return (
     <div className="phonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
       <span className="phoText">{props.phonetic.text}</span>
